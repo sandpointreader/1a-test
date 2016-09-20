80052215ee7d5b772d4540e254a8b2269bb6d564
sandpointreader <sandpointreader@gmail.com>
2016-09-20 08:51:42 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+
+</head>
+
+<body>
+	
+	<?php	
+	$lastname = "Sainsbury";
+	$firstname = "Robby";
+	print $firstname. " is my first name. ";
+	print $lastname. " is my last name";
+		
+	?>
+
+	
+	
+	
+</body>
+</html>
